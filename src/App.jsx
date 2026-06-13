08bbdc19962292466996b6e04ba4121dd5e6528c
siavashimrz <siavashi3899@gmail.com>
2026-06-13 13:21:24 +0330

    Make app grow height

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ToDoListItems from './ToDoListItems';
 
 function App() {
   const [listItem, setListItem] = useState(['Buy milk', 'Call Joe']);
+  const listLength = listItem.length;
+  const appHeight = listLength>9 ? ((listLength+7)*37) : 16*37;
 
   function handleAdd(item) {
     setListItem((prev) => [...prev, item]);
@@ -16,7 +18,7 @@ function App() {
   }
 
   return (
-    <div className="todo-app">
+    <div className="todo-app" style={{height:`${appHeight}px`}}>
       <h1 className="title">To-Do List</h1>
       <TextInput onAdd={handleAdd} />
       <ToDoListItems toDoList={listItem} onDelete={handleDelete} />
