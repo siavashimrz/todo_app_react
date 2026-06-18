e486a0755d09a76a964364a20dc7834684031c9c
siavashimrz <siavashi3899@gmail.com>
2026-06-18 17:46:06 +0330

    Make pin option work

--- src/ToDoListItems.jsx
+++ src/ToDoListItems.jsx
@@ -10,24 +10,41 @@ function Button({children, onClickEvent}){
   );
 }
 
-function ToDoListItems({ toDoList, onDelete, onIsDone, onIsPin }) {
+function ToDoListItem({ showPins, toDoList, onDelete, onIsDone, onIsPin }){
+  const filteredList = toDoList.filter(item => item.isPin === showPins);
+  const pinStyle = {
+    fontWeight: "900",
+    color: "red"
+  };
+
   return (
-    <ul>
-      {toDoList.map((item) => (
-        <li key={item.id} onClick={()=> onIsDone(item.id)}>
+    <>
+      {filteredList.map((item) => (
+        <li key={item.id} onClick={()=> onIsDone(item.id)} style={showPins ? pinStyle : null}>
         <div className="li-container">
             {item.isDone ? <del>{item.text}</del> : item.text}
             <Button onClickEvent={() => onDelete(item.id)}>
               <i className="fa-solid fa-trash-can"></i>
             </Button>
             <Button onClickEvent={() => onIsPin(item.id)}>
-              <i className="fa-solid fa-thumbtack"></i>
+              {showPins
+              ? <i class="fa-solid fa-thumbtack-slash"></i>
+              : <i className="fa-solid fa-thumbtack"></i>}
             </Button>
         </div>
       </li>
       ))}
-    </ul>
+    </>
   );
 }
 
-export default ToDoListItems;
+export default function ToDoListItems({ toDoList, onDelete, onIsDone, onIsPin }) {
+  return (
+    <ul>
+      <ToDoListItem showPins={true} toDoList={toDoList} onDelete={onDelete} onIsDone={onIsDone} onIsPin={onIsPin} />
+      
+      <ToDoListItem showPins={false} toDoList={toDoList} onDelete={onDelete} onIsDone={onIsDone} onIsPin={onIsPin} />
+      
+    </ul>
+  );
+}
